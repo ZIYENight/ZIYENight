5e171808494ed6a31aa653b320870c12e887a9a5
Night <102796030+ZIYENight@users.noreply.github.com>
2022-10-20 15:33:20 +0800

    Delete 初阶段文件 directory
    初阶段文件/ZiYe_Secure/ZiYe.ghpy

--- 初阶段文件/AMain.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*- 
-# @Time : 2022/4/19 14:53 
-# @Author : 子夜
-
-import clr
-
-clr.CompileModules("ZiYe.ghpy", "Brep.py", "Brep_off.py", "Data_list.py", "Line.py", "Object.py")
